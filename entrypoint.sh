9bafb91a492b7cf9b5c5e20fd9e12215b556d649
Yikun Jiang <yikunkero@gmail.com>
2021-06-04 17:07:17 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 set -x
 
 # Download Spark master code
-git init /spark-master-test-maven-arm
-cd /spark-master-test-maven-arm
+git init ~/spark-master-test-maven-arm
+cd ~/spark-master-test-maven-arm
 git --version
 git fetch --tags --progress -- https://github.com/apache/spark.git +refs/heads/*:refs/remotes/origin/* # timeout=30
 git config remote.origin.url https://github.com/apache/spark.git # timeout=30
@@ -12,7 +12,7 @@ git rev-parse origin/master^{commit} # timeout=30
 git checkout master
 
 # Build
-./build/mvn clean package -DskipTests -Paarch64 -Phadoop-2.7 -Pyarn -Phive -Phive-thriftserver -Pkinesis-asl -Pmesos
+# ./build/mvn clean package -DskipTests -Paarch64 -Phadoop-2.7 -Pyarn -Phive -Phive-thriftserver -Pkinesis-asl -Pmesos
 
 # Test
-./build/mvn test -fn -Paarch64 -Phadoop-2.7 -Pyarn -Phive -Phive-thriftserver -Pkinesis-asl -Pmesos
+./build/mvn test -fn -Paarch64 -Phadoop-2.7 -Pyarn -Phive -Phive-thriftserver -Pkinesis-asl -Pmesos -pl :spark-core_2.12,:spark-unsafe_2.12
