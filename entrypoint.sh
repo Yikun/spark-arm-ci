1d60bb5cf732e10cb7545a792915ed4a708ffe3e
Yikun Jiang <yikunkero@gmail.com>
2021-06-08 10:49:35 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,4 +13,5 @@ git rev-parse origin/master^{commit} # timeout=30
 git checkout master
 
 cd ~/spark-master-test-maven-arm
+./build/mvn clean package -DskipTests -Paarch64 -Phadoop-2.7 -Pyarn -Phive -Phive-thriftserver -Pkinesis-asl -Pmesos
 ./build/mvn test -Paarch64 -Phadoop-2.7 -Pyarn -Phive -Phive-thriftserver -Pkinesis-asl -Pmesos --fail-at-end -Dmaven.test.failure.ignore=true -pl :spark-sql_2.12,:spark-core_2.12
