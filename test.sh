c9547d145b0252def68a8a2d82bde95ea5b0696e
Yikun Jiang <yikunkero@gmail.com>
2021-08-04 16:54:09 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -ex
 
-./build/mvn -fn -Paarch64 -Phadoop-2.7 -Pyarn -Phive -Phive-thriftserver -Pkinesis-asl -Pmesos --fail-at-end -Dmaven.test.failure.ignore=true test -pl :spark-core_2.12,:spark-streaming_2.12,:spark-sql_2.12
+export MAVEN_OPTS="-Xss64m -Xmx2g -XX:ReservedCodeCacheSize=1g -Dorg.slf4j.simpleLogger.defaultLogLevel=WARN"
+export MAVEN_CLI_OPTS="--no-transfer-progress"
+./build/mvn $MAVEN_CLI_OPTS -Paarch64 -Phadoop-2.7 -Pyarn -Phive -Phive-thriftserver -Pkinesis-asl -Pmesos --fail-at-end -Dmaven.test.failure.ignore=true test -pl :spark-core_2.12,:spark-streaming_2.12,:spark-sql_2.12
