5accf89fd289fc90425a2ef5c9542ef59f0e68cb
Yikun Jiang <yikunkero@gmail.com>
2021-08-04 16:49:45 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -ex
 
-./build/mvn clean package -DskipTests -Paarch64 -Phadoop-2.7 -Pyarn -Phive -Phive-thriftserver -Pkinesis-asl -Pmesos
-
-./build/mvn -fn -Paarch64 -Phadoop-2.7 -Pyarn -Phive -Phive-thriftserver -Pkinesis-asl -Pmesos --fail-at-end test -pl :spark-core_2.12,:spark-streaming_2.12,:spark-sql_2.12
+./build/mvn -fn -Paarch64 -Phadoop-2.7 -Pyarn -Phive -Phive-thriftserver -Pkinesis-asl -Pmesos --fail-at-end -Dmaven.test.failure.ignore=true test -pl :spark-core_2.12,:spark-streaming_2.12,:spark-sql_2.12
